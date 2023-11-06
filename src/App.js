12370cc3e024dde660e965072da3b125297411d2
Kağan Değirmen <kagandegirmen@gmail.com>
2023-11-07 00:48:55 +0300

    todos, deleteTodo added

--- src/App.js
+++ src/App.js
@@ -1,7 +1,25 @@
+import React, { useState } from "react";
+import Todos from "./todos";
+
 function App() {
+  const [state, setState] = useState({
+    todos: [
+      { id: 1, content: "buy some milk" },
+      { id: 2, content: "play mario kart" },
+    ],
+  });
+
+  const deleteTodo = (id) => {
+    const newList = state.todos.filter((item) => item.id !== id);
+    console.log(newList);
+    var tmpState = { ...state };
+    tmpState.todos = newList;
+    setState(tmpState);
+  };
   return (
-    <div className="App">
-      <h2>Hello</h2>
+    <div className="todo-app container">
+      <h1 className="center blue-text">Todo's</h1>
+      <Todos todos={state.todos} deleteTodo={deleteTodo} />
     </div>
   );
 }
